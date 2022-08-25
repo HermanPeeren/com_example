4413a9a399e0634cc7d37967188210a8185cc35d
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2022-08-25 21:20:15 +0300

    Use the proposed \Joomla\CMS\MVC\Factory\MVCFactoryWrapper

--- component/backend/src/Service/MVCFactoryWrapper.php
+++ component/backend/src/Service/MVCFactoryWrapper.php
@@ -17,37 +17,9 @@ use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use Joomla\CMS\MVC\Model\ModelInterface;
 use Joomla\Input\Input;
 
-class MVCFactoryWrapper implements FoobarAware, MVCFactoryInterface
+class MVCFactoryWrapper extends \Joomla\CMS\MVC\Factory\MVCFactoryWrapper implements FoobarAware
 {
 	use FoobarAwareTrait;
-	use MVCFactoryAwareTrait;
-
-	public function __construct(MVCFactoryInterface $factory)
-	{
-		$this->setMVCFactory($factory);
-	}
-
-	/**
-	 * Method to load and return a controller object.
-	 *
-	 * @param   string                   $name    The name of the controller
-	 * @param   string                   $prefix  The controller prefix
-	 * @param   array                    $config  The configuration array for the controller
-	 * @param   CMSApplicationInterface  $app     The app
-	 * @param   Input                    $input   The input
-	 *
-	 * @return  \Joomla\CMS\MVC\Controller\ControllerInterface
-	 *
-	 * @since   1.0.0
-	 * @throws  \Exception
-	 */
-	public function createController($name, $prefix, array $config, CMSApplicationInterface $app, Input $input)
-	{
-		/** @var BaseController $controller */
-		$controller = $this->getMVCFactory()->createController($name, $prefix, $config, $app, $input);
-
-		return $controller;
-	}
 
 	/**
 	 * Method to load and return a model object.
@@ -71,40 +43,5 @@ class MVCFactoryWrapper implements FoobarAware, MVCFactoryInterface
 		}
 
 		return $model;
-	}
-
-	/**
-	 * Method to load and return a view object.
-	 *
-	 * @param   string  $name    The name of the view.
-	 * @param   string  $prefix  Optional view prefix.
-	 * @param   string  $type    Optional type of view.
-	 * @param   array   $config  Optional configuration array for the view.
-	 *
-	 * @return  \Joomla\CMS\MVC\View\ViewInterface  The view object
-	 *
-	 * @since   1.0.0
-	 * @throws  \Exception
-	 */
-	public function createView($name, $prefix = '', $type = '', array $config = [])
-	{
-		return $this->getMVCFactory()->createView($name, $prefix, $type, $config);
-	}
-
-	/**
-	 * Method to load and return a table object.
-	 *
-	 * @param   string  $name    The name of the table.
-	 * @param   string  $prefix  Optional table prefix.
-	 * @param   array   $config  Optional configuration array for the table.
-	 *
-	 * @return  \Joomla\CMS\Table\Table  The table object
-	 *
-	 * @since   1.0.0
-	 * @throws  \Exception
-	 */
-	public function createTable($name, $prefix = '', array $config = [])
-	{
-		return $this->getMVCFactory()->createTable($name, $prefix, $config);
 	}
 }
